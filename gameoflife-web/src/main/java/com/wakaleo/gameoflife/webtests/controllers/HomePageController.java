e83e5b011ef7f3428c2bde854cee60a8cc757810
upasana1808 <56124444+upasana1808@users.noreply.github.com>
2020-08-11 08:00:28 +0530

    Update HomePageController.java

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/HomePageController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/HomePageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
  /**
  * Wakaleo Consulting - John Ferguson Smart
  * Game of life, demonstration application for Jenkins: The Definitive Guide
- *
+ *  Upasana's code
  * HomePageController.java
  * Controller to navigate to the main page
  */
